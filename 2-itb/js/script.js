188c13d62fd877594f060b9e2a6e32f6273cf347
PhoebeJao <60592419+PhoebeJao@users.noreply.github.com>
2020-02-24 12:38:20 +0800

    commit

---
+++ 2-itb/js/script.js
@@ -0,0 +1,6 @@
+let person = {name:"juan dela cruz", gender:"male", age:25};
+let person2 = {name:"cathy garcia", gender:"female", age:20};
+
+console.log(person.age);
+console.log(`${person.name} is a ${person.gender} person whose age is ${person.age}`);
+console.log(`${person2.name} is a ${person2.gender} person whose age is ${person2.age}`);
